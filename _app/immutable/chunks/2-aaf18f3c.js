73f073574277242c3355e7d18f67bf02d27f1bc4
Bastian <bastianoppermann1997@gmail.com>
2022-12-06 21:30:15 +0000

    Deploying to gh-pages from @ Bastian/dapper-ui@44cb879ea4746c3d30b959ca6cddfcbe787847c9 🚀

---
+++ _app/immutable/chunks/2-aaf18f3c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-6d156a2d.js";export{t as component};
--- _app/immutable/chunks/2-c69c3041.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c036a39c.js";export{t as component};
