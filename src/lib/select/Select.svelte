b58fd24391c9556912c80a6359a8e79f36d656d0
Bastian Oppermann <git@bastian-oppermann.de>
2022-12-06 20:09:03 +0100

    Fix select text positioning

--- src/lib/select/Select.svelte
+++ src/lib/select/Select.svelte
@@ -46,15 +46,15 @@
 
 <style lang="postcss" global>
 	:local(select) {
-		@apply rounded border-gray-300 text-gray-700 focus:border-primary-300 focus:ring-primary-700/70 dark:border-dark-400 dark:bg-dark-600 dark:text-gray-200 dark:focus:border-primary-600;
+		@apply rounded border-gray-300 pt-0 pb-0 text-gray-700 focus:border-primary-300 focus:ring-primary-700/70 dark:border-dark-400 dark:bg-dark-600 dark:text-gray-200 dark:focus:border-primary-600;
 		background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3e%3cpath stroke='%2364748b' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3e%3c/svg%3e");
 	}
 
 	:local(.size-base) {
-		@apply h-10 pt-1.5;
+		@apply h-10;
 	}
 
 	:local(.size-sm) {
-		@apply h-9 pt-1;
+		@apply h-9;
 	}
 </style>
