8b524160b5a3973ed982cb5d8326931c56f6644b
Bastian Oppermann <git@bastian-oppermann.de>
2022-12-08 20:41:54 +0100

    Apply theme globally

--- src/lib/ThemeProvider.svelte
+++ src/lib/ThemeProvider.svelte
@@ -11,21 +11,19 @@
 		};
 	}
 
-	function colorPaletteToCssVar(palette: ColorPalette, prefix: string) {
+	function colorPaletteToCssVar(palette: ColorPalette, prefix: string): string[] {
 		const n = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
-		const cssVar = palette.reduce((acc, color, index) => {
+		const cssVar = palette.map((color, index) => {
 			const rgb = hexToRgb(color);
 			const rgbString = `${rgb.r} ${rgb.g} ${rgb.b}`;
-			return `${acc}--d4r-color-${prefix}-${n[index]}: ${rgbString};`;
-		}, '');
+			return `--d4r-color-${prefix}-${n[index]}: ${rgbString};`;
+		});
 
 		return cssVar;
 	}
 
-	function colorsToCssVar(colors: Record<string, ColorPalette>) {
-		return Object.entries(colors).reduce((acc, [key, value]) => {
-			return `${acc}${colorPaletteToCssVar(value, key)}`;
-		}, '');
+	function colorsToCssVar(colors: Record<string, ColorPalette>): string[] {
+		return Object.entries(colors).flatMap(([key, value]) => colorPaletteToCssVar(value, key));
 	}
 
 	export type Radius = 'none' | 'sm' | 'base' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'full';
@@ -120,26 +118,26 @@
 		}
 	};
 
-	function getRadiusCssVars(theme: DapperUiTheme) {
+	function getRadiusCssVars(theme: DapperUiTheme): string[] {
 		function mapBorderRadius(from: string, to: string) {
 			return `--d4r-border-radius-${from}: var(--d4r-border-radius-${to});`;
 		}
-		return (
-			'--d4r-border-radius-none: 0px;' +
-			'--d4r-border-radius-sm: 0.125rem;' +
-			'--d4r-border-radius-base: 0.25rem;' +
-			'--d4r-border-radius-md: 0.375rem;' +
-			'--d4r-border-radius-lg: 0.5rem;' +
-			'--d4r-border-radius-xl: 0.75rem;' +
-			'--d4r-border-radius-2xl: 1rem;' +
-			'--d4r-border-radius-3xl: 1.5rem;' +
-			'--d4r-border-radius-full: 9999px;' +
-			mapBorderRadius('button', theme.radiuses.button) +
-			mapBorderRadius('input', theme.radiuses.input) +
-			mapBorderRadius('checkbox', theme.radiuses.checkbox) +
-			mapBorderRadius('slider-thumb', theme.radiuses.slider.thumb) +
+		return [
+			'--d4r-border-radius-none: 0px;',
+			'--d4r-border-radius-sm: 0.125rem;',
+			'--d4r-border-radius-base: 0.25rem;',
+			'--d4r-border-radius-md: 0.375rem;',
+			'--d4r-border-radius-lg: 0.5rem;',
+			'--d4r-border-radius-xl: 0.75rem;',
+			'--d4r-border-radius-2xl: 1rem;',
+			'--d4r-border-radius-3xl: 1.5rem;',
+			'--d4r-border-radius-full: 9999px;',
+			mapBorderRadius('button', theme.radiuses.button),
+			mapBorderRadius('input', theme.radiuses.input),
+			mapBorderRadius('checkbox', theme.radiuses.checkbox),
+			mapBorderRadius('slider-thumb', theme.radiuses.slider.thumb),
 			mapBorderRadius('slider-track', theme.radiuses.slider.track)
-		);
+		];
 	}
 </script>
 
@@ -177,6 +175,12 @@
 	let className = '';
 	export { className as class };
 
+	/**
+	 * When set to true, the theme will not be applied locally but only
+	 * for child components.
+	 */
+	export let localStyle = false;
+
 	export let theme: DapperUiTheme = defaultDapperUiTheme;
 
 	const themeStore = writable(theme);
@@ -188,6 +192,25 @@
 	);
 </script>
 
-<div class={className} style={`${colorsToCssVar(theme.colors)}${getRadiusCssVars(theme)}`}>
+<svelte:head>
+	{#if !localStyle}
+		<!-- MAKE GOD DAMN SURE THAT EVERYTHING IN THERE IS PROPERLY ESCAPED. 
+		IT CURRENTLY ISN'T, SO USER SHOULD NOT BE ALLOWED TO PROVIDE CUSTOM THEMES -->
+		{@html `<` +
+			`style type="text/css">:root {\n${colorsToCssVar(theme.colors).join(
+				'\n'
+			)}\n${getRadiusCssVars(theme).join('\n')}
+	</style>`}
+	{/if}
+</svelte:head>
+
+{#if localStyle}
+	<div
+		class={className}
+		style="{colorsToCssVar(theme.colors).join('')}{getRadiusCssVars(theme).join('')}"
+	>
+		<slot />
+	</div>
+{:else}
 	<slot />
-</div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,10 @@
 			// input: 'none'
 		}
 	}}
-	class="d4r-h-full d4r-overflow-y-auto d4r-bg-neutral-100 d4r-p-4 dark:d4r-bg-dark-700 sm:d4r-p-6 md:d4r-p-8 lg:d4r-p-16"
 >
-	<div class="d4r-container d4r-mx-auto"><slot /></div>
+	<div
+		class="d4r-overflow-y-auto d4r-bg-neutral-100 d4r-p-4 dark:d4r-bg-dark-700 sm:d4r-p-6 md:d4r-p-8 lg:d4r-p-16"
+	>
+		<div class="d4r-container d4r-mx-auto"><slot /></div>
+	</div>
 </ThemeProvider>
