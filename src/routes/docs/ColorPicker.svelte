eb901c112bedf4963a28a1d92ec2d8cadb7b0db8
Bastian Oppermann <git@bastian-oppermann.de>
2022-12-13 19:18:49 +0100

    Match ColorPicker label style with other labels

--- src/routes/docs/ColorPicker.svelte
+++ src/routes/docs/ColorPicker.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="d4r-space-y-1 d4r-text-sm d4r-font-medium d4r-text-neutral-700">
-	<Text contrast="lower" size="sm" weight="semibold">{label}</Text>
+	<Text contrast="low" size="sm">{label}</Text>
 	<div class="d4r-flex d4r-max-w-sm d4r-flex-wrap d4r-gap-1">
 		{#each ['gray', 'red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose'] as c}
 			<IconButton
