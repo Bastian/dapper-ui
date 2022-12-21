e74fca460f2ce6a8af00c95a95e2e5324f3c3230
Bastian Oppermann <git@bastian-oppermann.de>
2022-12-21 21:36:48 +0100

    Fix misaligned line numbers
    It was caused by a different line height in the code block and for the
    line numbers.

--- src/doc-components/code-highlight/StyledCodeBlock.svelte
+++ src/doc-components/code-highlight/StyledCodeBlock.svelte
@@ -16,7 +16,9 @@
 	<div
 		class="d4r-mt-2 d4r-select-none d4r-border-r d4r-border-neutral-200 d4r-px-4 d4r-text-sm d4r-text-neutral-500 dark:d4r-border-dark-600"
 	>
-		<pre>{[...Array(code.split('\n').length)].map((_, i) => `${i + 1}`).join('\n')}</pre>
+		<pre><code class="language-{language}"
+				>{[...Array(code.split('\n').length)].map((_, i) => `${i + 1}`).join('\n')}</code
+			></pre>
 	</div>
 	<div class="d4r-overflow-x-auto">
 		<CodeBlock class="d4r-py-2" {language} {code} {noDedent} />
