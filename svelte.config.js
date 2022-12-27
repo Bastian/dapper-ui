8245255295ef12e75f678aee634fdcff96be56d7
Bastian Oppermann <git@bastian-oppermann.de>
2022-12-27 13:46:18 +0100

    Add Cloudflare Deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,10 +8,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/dapper-ui'
-		}
+		adapter: adapter()
 	},
 
 	package: {
