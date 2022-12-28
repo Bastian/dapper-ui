586046e7e818539a2dc39a4034ae5fcadaeba2d5
Bastian Oppermann <git@bastian-oppermann.de>
2022-12-28 12:25:17 +0100

    Add legal links

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,9 +1,16 @@
 <script>
+	import { base } from '$app/paths';
 	import Text from '$lib/components/text/Text.svelte';
 </script>
 
 <footer
-	class="d4r-flex d4r-items-center d4r-justify-center d4r-border-t d4r-border-t-neutral-200 d4r-bg-white d4r-px-8 d4r-py-4 dark:d4r-border-t-dark-600 dark:d4r-bg-dark-800"
+	class="d4r-flex d4r-flex-col d4r-items-center d4r-justify-center d4r-gap-3 d4r-border-t d4r-border-t-neutral-200 d4r-bg-white d4r-px-8 d4r-py-4 dark:d4r-border-t-dark-600 dark:d4r-bg-dark-800"
 >
-	<Text contrast="lower">© 2022 Bastian Oppermann</Text>
+	<div>
+		<Text>© {new Date().getFullYear()} Dapper UI</Text>
+	</div>
+	<div class="d4r-flex d4r-gap-3">
+		<Text contrast="lower" size="sm" tag="a" href="{base}/privacy-policy">Privacy Policy</Text>
+		<Text contrast="lower" size="sm" tag="a" href="{base}/imprint">Imprint</Text>
+	</div>
 </footer>
---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Text from '$lib/components/text/Text.svelte';
+	import Footer from '../Footer.svelte';
+</script>
+
+<div class="d4r-flex d4r-h-full d4r-flex-col">
+	<div class="d4r-container d4r-mx-auto d4r-flex-grow d4r-overflow-x-auto d4r-p-8 d4r-py-16">
+		<Text tag="h1" weight="bold" size="lg">Imprint</Text>
+		<iframe
+			title="Imprint"
+			class="d4r-h-full d4r-w-full d4r-min-w-[500px] d4r-rounded d4r-bg-white"
+			src={import.meta.env.VITE_IMPRINT_URL}
+		/>
+	</div>
+
+	<Footer />
+</div>
